43993414bc8cfaeb8d6b2ae9fc50c5d1faa48d49
Ibrahima <114165898+Cordapeche@users.noreply.github.com>
2023-03-02 22:49:01 +0100

    add

---
+++ tuto3 - Import-Export - Copy/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/anyname';
+import hello from './components/hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <hello />
+    </div>
+  );
+}
+
+export default App;
